c9893abd25e57e45b48a1550b20923295c92fdce
Dijar06 <dize06001@utb.vaxjo.se>
2023-10-23 14:56:44 +0200

    Add project files.

---
+++ Uppgift 4.2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Uppgift4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int storstaTal = 0;
+
+            Console.WriteLine("Skriv in ett heltal: ");
+            int tal1 = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine("Skriv ett till heltal, eller avsluta med 0");
+                int tal2 = int.Parse(Console.ReadLine());
+
+                if (tal1 > storstaTal)
+                {
+                    storstaTal = tal1;
+                }
+
+                else if (tal2 > storstaTal)
+                {
+                    storstaTal = tal2;
+                }
+                
+                else if (tal2 == 0)
+                {
+                    break;
+                }
+
+            } while (true);
+
+            Console.WriteLine("Programmet har avslutats. Det största talet du skrev in är: " + storstaTal);
+        }
+    }
+}
